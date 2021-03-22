e4a207b4e4cde2a17f2bce37b64545f7f350bd56
unknown <marouanemiramax4@gmail.com>
2021-03-22 14:36:11 -0300

    atualizacao

---
+++ doafood/doafood/src/main/java/br/com/doafood/doafood/DoafoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.doafood.doafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoafoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoafoodApplication.class, args);
+	}
+
+}
---
+++ doafood/doafood/src/main/java/br/com/doafood/doafood/model/Comunidade.java
@@ -0,0 +1,75 @@
+package br.com.doafood.doafood.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_comunidade")
+public class Comunidade {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	@Size(min = 6, max = 50)
+	private String nome;
+	@NotNull
+	@Size(min = 50, max = 250)
+	private String descricao;
+	@NotNull
+	@Size(min = 6, max = 100)
+	private String bairro;
+	/*@ManyToOne
+	@JsonIgnoreProperties("comunidade")
+	private List<Doador> doador;
+
+	public List<Doador> getDoador() {
+		return doador;
+	}
+
+	public void setDoador(List<Doador> doador) {
+		this.doador = doador;
+	}*/
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+}
