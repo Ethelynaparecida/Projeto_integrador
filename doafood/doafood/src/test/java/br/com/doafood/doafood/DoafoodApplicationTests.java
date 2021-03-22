037c0715ee836150d20e88ab40cffaaab2a3282f
Nivea <nivanina.martins@gmail.com>
2021-03-22 11:35:32 -0300

    arquivosmaven

---
+++ doafood/doafood/src/test/java/br/com/doafood/doafood/DoafoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doafood.doafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoafoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
