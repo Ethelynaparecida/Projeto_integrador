fb533811f29dc1107cb6f9fa0a06bd301fa84447
Nivea <nivanina.martins@gmail.com>
2021-03-25 09:23:54 -0300

    envio de crud

---
+++ doafood/doafood/src/main/java/br/com/doafood/doafood/controller/ComunidadeController.java
@@ -0,0 +1,59 @@
+package br.com.doafood.doafood.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.doafood.doafood.model.Comunidade;
+import br.com.doafood.doafood.repository.ComunidadeRepository;
+
+@RestController
+@RequestMapping("/categoria")
+@CrossOrigin(origins = "*" , allowedHeaders = "*")
+public class ComunidadeController {
+	@Autowired
+	private ComunidadeRepository repository;
+	
+	@GetMapping
+	private ResponseEntity<List<Comunidade>> findAllCategoria(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	private ResponseEntity<Comunidade> findByIDCategoria(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).
+				orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/descricao/{descricao}")
+	private ResponseEntity<List<Comunidade>> findByDescricaoCategoria(@PathVariable String descricao){
+		return ResponseEntity.ok(repository.findByDescricaoContainingIgnoreCase(descricao));
+	}
+	
+	@PostMapping
+	public ResponseEntity <Comunidade> postCategoria (@RequestBody Comunidade categoria){
+		return ResponseEntity.status(HttpStatus.CREATED).
+				body(repository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity <Comunidade> putCategoria (@RequestBody Comunidade categoria){
+		return ResponseEntity.ok(repository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteCategoria(@PathVariable long id){
+		repository.deleteById(id);
+	}
+}
---
+++ doafood/doafood/src/main/java/br/com/doafood/doafood/repository/ComunidadeRepository.java
@@ -0,0 +1,14 @@
+package br.com.doafood.doafood.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.doafood.doafood.model.Comunidade;
+
+@Repository
+public interface ComunidadeRepository extends JpaRepository<Comunidade, Long>{
+	public List<Comunidade>findAllByNomeContainingIgnoreCase (String Nome);
+	public List<Comunidade>findByDescricaoContainingIgnoreCase(String descricao);
+}
